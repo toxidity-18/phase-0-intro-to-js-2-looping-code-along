69ce784b5b070edb3c1cd7970768e8f7b50482bb
Samuel <samuel.kamawira@student.moringaschool.com>
2024-07-07 11:29:05 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const sinon = require('sinon');
+function writeCards(names, occasion) {
+  const messages = [];
+  for (const name of names) {
+    messages.push(`Thank you, ${name}, for the wonderful ${occasion} gift!`);
+  }
+  return messages;
+}
+function countDown(startNumber) {
+  for (let i = startNumber; i >= 0; i--) {
+    console.log(i);
+  }
+}
